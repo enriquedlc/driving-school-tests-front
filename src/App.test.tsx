c8015bd96b590857a27ca87305268327a2bf3a1b
enriquedlc <enriquedelaconcepcion@itop.es>
2024-05-01 21:43:25 +0100

    :wrench: removed unnecessary stuff

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,12 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import App from "./App";
 
 describe("App", () => {
-  it("should render the app", async () => {
+  it("render driving tests text", async () => {
     render(<App />);
-    userEvent.click(screen.getByText("count is 0"));
-    await waitFor(() => expect(screen.getByText("count is 1")));
+    const text = await screen.findByText(/driving tests/i);
+    expect(text).toBeInTheDocument();
   });
 });
