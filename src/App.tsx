0c97ede93b2baaf757614b6c9fb7b8a4df606796
humberDEV <humberbrawl@gmail.com>
2024-05-01 23:15:41 +0200

    :input_latin_letters: font config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <>driving tests</>;
+  return (
+    <>
+      <p className="poppins-regular-italic">driving tests</p>
+    </>
+  );
 }
 
 export default App;
